e3696531c6fb86415d7ce586afb3fd8ddb4df127
SK-Yeboah <skyeboah9@gmail.com>
2024-03-08 14:42:20 -0800

    web_Static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Sets up a web server for deployment of web_static.
 
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+apt-get update
+apt-get install -y nginx
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/{releases/test,shared}
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create a fake HTML file for testing
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-# Give ownership of the /data/ folder to the ubuntu user AND group recursively
-sudo chown -R ubuntu:ubuntu /data/
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-# Update Nginx configuration to serve content of /data/web_static/current/ to hbnb_static
-CONFIG_FILE="/etc/nginx/sites-available/default"
-sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' "$CONFIG_FILE"
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
 
-# Restart Nginx to apply changes
-sudo service nginx restart
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-exit 0
-
+service nginx restart
