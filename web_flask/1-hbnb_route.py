c85243789f68e53a5aa812ab6a1e3380fa22cd2b
SK-Yeboah <skyeboah9@gmail.com>
2024-04-06 03:12:35 -1200

    application

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,24 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 """
-script that starts a Flask web application 
+script that starts a Flask web application
 listening on 0.0.0.0, port 5000
 """
 
-from flask import Flask;
+from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     return 'HBNB'
 
-#Run flash
+
+# Run flash
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
